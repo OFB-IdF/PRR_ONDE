ea8cd179e01d89e6190d5c8672d5824c01d69742
CedricMondy <cedric.mondy@gmail.com>
2024-01-04 17:29:59 +0100

    installation séparée de {sf}

--- 00_installation_packages.R
+++ 00_installation_packages.R
@@ -1,13 +1,15 @@
 ## Dépendances 
 
-pkg_gh <- c("richaben/ondetools", "inrae/hubeau")
-pkg_cran <- c("tidyverse", "purrr", "sf", "mapview", "leaflet", "leaflet.extras", "ggrepel", "glue", "forcats", "scales", "data.table", "lubridate", "stringr", "tidyr", "ggplot2", "knitr", "rmarkdown", "htmltools", "leafem", "png", "webp")
+# pkg_gh <- c("richaben/ondetools", "inrae/hubeau")
+# pkg_cran <- c("tidyverse", "purrr", "sf", "mapview", "leaflet", "leaflet.extras", "ggrepel", "glue", "forcats", "scales", "data.table", "lubridate", "stringr", "tidyr", "ggplot2", "knitr", "rmarkdown", "htmltools", "leafem", "png", "webp")
+# 
+# try(pak::pkg_install(c(pkg_gh, pkg_cran)))
+# 
+# sapply(
+#   X = pkg_cran,
+#   FUN = function(pkg) {
+#     if (!require(pkg, character.only = TRUE)) install.packages(pkg)
+#     }
+#   )
 
-try(pak::pkg_install(c(pkg_gh, pkg_cran)))
-
-sapply(
-  X = pkg_cran,
-  FUN = function(pkg) {
-    if (!require(pkg, character.only = TRUE)) install.packages(pkg)
-    }
-  )
+install.packages("sf")
